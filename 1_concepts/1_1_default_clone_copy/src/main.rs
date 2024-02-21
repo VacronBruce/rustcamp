be0336fa0a7412d1d95610aee5cb9486525ac16c
Bruce <homan311@gmail.com>
2024-02-21 00:10:16 -0500

    finish default clone copy task

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,43 @@
+use std::default;
+
+use smart_default::SmartDefault;
+
+#[derive(Clone, Copy, SmartDefault)]
+struct Point {
+    #[default = 100.0]
+    x: f64,
+    #[default = 2000.0]
+    y: f64,
+}
+#[derive(Clone)]
+struct Polyline {
+    points: Vec<Point>,
+}
+
+impl Polyline {
+    fn new(points: Vec<Point>) -> Self {
+        Self {
+            points
+        }
+    }
+
+    fn add(&mut self, item: Point) {
+        self.points.push(item)
+    }
+
+    fn get(&self, i: usize) -> Option<&Point> {
+        self.points.get(i)
+    }
+
+    fn remove(&mut self, i: usize) -> Option<Point> {
+        if i >= self.points.len() {
+            return None;
+        }
+        return Some(self.points.remove(i))
+    }
+}
+
+
 fn main() {
-    println!("Implement me!");
+    println!("Hello, world!");
 }
